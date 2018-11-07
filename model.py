fee43f073b9fd2b8b17812a196ccd4d0b1004fad
eliasrosendahl <jensen.eliasrosendahl@gmail.com>
2018-11-07 16:09:39 +0100

    Shows score

--- model.py
+++ model.py
@@ -7,6 +7,7 @@ class Model(object):
     def __init__(self):
         self.snake = Snake()
         self.food = [Food(10, 10)]
+        self.score = 0
 
     def update(self):
         # Checks if snake can eat the food
@@ -15,6 +16,7 @@ class Model(object):
                 self.food.remove(food)
                 self.food.append(Food(random.randrange(20), random.randrange(20)))
                 self.snake.eat()
+                self.score += 1
                 break
 
         self.snake.move()
--- view.py
+++ view.py
@@ -15,14 +15,19 @@ class View(object):
         self.screen = pygame.display.set_mode(size)
         pygame.display.set_caption("snake")
 
+        self.font = pygame.font.SysFont("Sans Seriff", 42)
+
     def update(self, model):
         self.screen.fill(self.WHITE)
+
+        # Draw snake head
         pygame.draw.rect(self.screen, self.BLACK, pygame.Rect(
             model.snake.head.x * self.PIXELSIZE, 
             model.snake.head.y * self.PIXELSIZE, 
             self.PIXELSIZE, 
             self.PIXELSIZE))
 
+        # Draw snake body
         for part in model.snake.parts:
             pygame.draw.rect(self.screen, self.BLACK, pygame.Rect(
                 part.x * self.PIXELSIZE, 
@@ -30,11 +35,17 @@ class View(object):
                 self.PIXELSIZE, 
                 self.PIXELSIZE))
 
+        # Draw food
         for food in model.food:
             pygame.draw.rect(self.screen, self.RED, pygame.Rect(
                 food.x * self.PIXELSIZE,
                 food.y * self.PIXELSIZE,
                 self.PIXELSIZE,
                 self.PIXELSIZE))
+
+        # Show score
+        scoreString = "Score: " + str(model.score)
+        scoreText = self.font.render(scoreString, True, (0, 128, 0))
+        self.screen.blit(scoreText, (0, 0))
 
         pygame.display.flip()
