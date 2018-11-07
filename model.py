3caa5bbdff561537398bab03016d611315111c17
eliasrosendahl <jensen.eliasrosendahl@gmail.com>
2018-11-07 16:30:59 +0100

    snake now loops around edges

--- model.py
+++ model.py
@@ -8,6 +8,10 @@ class Model(object):
         self.snake = Snake()
         self.food = [Food(10, 10)]
         self.score = 0
+        
+        #Size of level
+        self.max_x = 45
+        self.max_y = 30
 
     def update(self):
         # Checks if snake can eat the food
@@ -25,3 +29,13 @@ class Model(object):
         for part in self.snake.parts:
             if part.x == self.snake.head.x and part.y == self.snake.head.y:
                 self.snake.alive = False
+
+        # Checks if snake is out of bounds
+        if self.snake.head.x >= self.max_x:
+            self.snake.head.x = 0
+        elif self.snake.head.x < 0:
+            self.snake.head.x = self.max_x
+        elif self.snake.head.y >= self.max_y:
+            self.snake.head.y = 0
+        elif self.snake.head.y < 0:
+            self.snake.head.y = self.max_y
